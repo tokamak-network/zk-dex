ea580576b6c883c98217b8565eec08bfb6368ccf
4000D <dkdkajej@gmail.com>
2019-07-14 19:49:03 +0900

    contracts/Market: add first draft for makeOrder()

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -19,6 +19,9 @@ contract Market is ZkDai {
     OrderState state;
   }
 
+  event OrderCreated(bytes32 orderHash, bytes32 indexed sourceToken, bytes32 indexed  targetToken);
+
+  mapping(bytes32 => Order) public orders;
 
   function makeOrder(
     bytes32 makerViewingKey,
@@ -27,13 +30,50 @@ contract Market is ZkDai {
     bytes32 targetToken,
     uint price
   ) external {
+    // TODO: verify circuit:makeOrder
 
+    require(sourceToken != targetToken, "Market: cannot make an order with same token pair");
+    require(notes[makerNote] == State.Committed, "Market: maker note is not available");
+
+    // TODO: instnatiate with zk-SNARK proofs
+    Order memory order = Order({
+      makerViewingKey: makerViewingKey,
+      makerNote: makerNote,
+      sourceToken: sourceToken,
+      price: price,
+      state: OrderState.Created
+    });
+
+    bytes32 orderHash = hashOrder(order);
+
+    // save order
+    orders[orderHash] = order;
+
+    // mark maker note as spent
+    notes[makerNote] = State.Spent;
+
+    emit OrderCreated(orderHash, sourceToken, targetToken);
   }
-  function takeOrder() external {
 
+  function takeOrder(
+    bytes32 orderHash,
+    bytes32 takerNoteToMaker
+  ) external {
+    Order storage order = orders[orderHash];
   }
   function settleOrder() external {
 
+  }
+
+    // TODO: use zk-SNARK proofs
+  function hashOrder(Order storage order) internal view returns (bytes32) {
+    return keccak256(abi.encode(
+      order.makerViewingKey,
+      order.makerNote,
+      order.sourceToken,
+      order.targetToken,
+      order.price
+    ));
   }
 }
 
