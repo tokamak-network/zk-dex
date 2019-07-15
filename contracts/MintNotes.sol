2ea59b23b099e6f2bd096f541d2e296da070bf62
4000D <dkdkajej@gmail.com>
2019-07-16 02:55:06 +0900

    contracts: re-indent

--- contracts/MintNotes.sol
+++ contracts/MintNotes.sol
@@ -12,27 +12,27 @@ contract MintNotes is MintNoteVerifier, ZkDaiBase {
   *      submission time, type, public inputs of the zkSnark and the submitter
   */
   function submit(
-      uint256[2] a,
-      uint256[2] a_p,
-      uint256[2][2] b,
-      uint256[2] b_p,
-      uint256[2] c,
-      uint256[2] c_p,
-      uint256[2] h,
-      uint256[2] k,
-      uint256[4] input,
-      bytes memory encryptedNote
+    uint256[2] a,
+    uint256[2] a_p,
+    uint256[2][2] b,
+    uint256[2] b_p,
+    uint256[2] c,
+    uint256[2] c_p,
+    uint256[2] h,
+    uint256[2] k,
+    uint256[4] input,
+    bytes memory encryptedNote
   )
     internal
   {
-      require(development || mintVerifyTx(a, a_p, b, b_p, c, c_p, h, k, input), "Failed to verify circuit");
+    require(development || mintVerifyTx(a, a_p, b, b_p, c, c_p, h, k, input), "Failed to verify circuit");
 
-      bytes32 note = calcHash(input[0], input[1]);
-      require(notes[note] == State.Invalid, "Note was already minted");
+    bytes32 note = calcHash(input[0], input[1]);
+    require(notes[note] == State.Invalid, "Note was already minted");
 
-      notes[note] = State.Committed;
-      encryptedNotes[note] = encryptedNote;
+    notes[note] = State.Committed;
+    encryptedNotes[note] = encryptedNote;
 
-      emit NoteStateChange(note, State.Committed);
+    emit NoteStateChange(note, State.Committed);
   }
 }
