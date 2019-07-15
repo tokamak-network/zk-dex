f7aada0ac8354b87b2a9143f7a750a29de1b88f7
Jin <jin.s@onther.io>
2019-07-15 17:08:15 +0900

    scripts: fiexd names to be deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,15 +1,15 @@
-var createNote_verifier = artifacts.require("./createNote-verifier.sol");
-var liquidateNote_verifier = artifacts.require("./liquidateNote-verifier.sol");
-var makeOrder_verifier = artifacts.require("./makeOrder-verifier.sol");
-var takeOrder_verifier = artifacts.require("./takeOrder-verifier.sol");
-var settleOrder_verifier = artifacts.require("./settleOrder-verifier.sol");
-var transferNote_verifier = artifacts.require("./transferNote-verifier.sol");
+var createNote_verifier = artifacts.require("./createNoteVerifier.sol");
+var liquidateNote_verifier = artifacts.require("./liquidateNoteVerifier.sol");
+var makeOrder_verifier = artifacts.require("./makeOrderVerifier.sol");
+var takeOrder_verifier = artifacts.require("./takeOrderVerifier.sol");
+var settleOrder_verifier = artifacts.require("./settleOrderVerifier.sol");
+var transferNote_verifier = artifacts.require("./transferNoteVerifier.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(createNote-erifier);
-  deployer.deploy(liquidateNote-verifier);
-  deployer.deploy(makeOrder-verifier);
-  deployer.deploy(takeOrder-verifier);
-  deployer.deploy(settleOrder-verifier);
-  deployer.deploy(transferNote-verifier);
+  deployer.deploy(createNote_verifier);
+  deployer.deploy(liquidateNote_verifier);
+  deployer.deploy(makeOrder_verifier);
+  deployer.deploy(takeOrder_verifier);
+  deployer.deploy(settleOrder_verifier);
+  deployer.deploy(transferNote_verifier);
 };
