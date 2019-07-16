1ac5c6fa4832db15fc527d54551340fb7a874ec5
Jin <jin.s@onther.io>
2019-07-17 05:41:48 +0900

    circuit : generatee proof script no need fixing proof.json anymore.

--- circuit/mintNBurnNote/genProof.sh
+++ circuit/mintNBurnNote/genProof.sh
@@ -5,8 +5,11 @@ then
 else
   ./zokrates compute-witness -a ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9} ${10} > "$(date +%FT%T)_compute_witness.log"
   ./zokrates generate-proof --proving-scheme pghr13 > "$(date +%FT%T)_generate_proof.log"
+
+  # Next line for fixing 'input' field of proof.json, but zokrates 4.9 version no longer necessary fixing.
   # get line no of Input field
-  export noInput=$(cat proof.json | sed -n '/input/ =')
-  cat proof.json | sed "${noInput}s/\[/\['0x/g" | sed "${noInput}s/\]/'\]/g" | sed "${noInput}s/\,/', '0x/g" | sed "s/'/\"/g" > fixed_proof.json
-  cat fixed_proof.json
+  # export noInput=$(cat proof.json | sed -n '/input/ =')
+  # cat proof.json | sed "${noInput}s/\[/\['0x/g" | sed "${noInput}s/\]/'\]/g" | sed "${noInput}s/\,/', '0x/g" | sed "s/'/\"/g" > mintNBurnProof.json
+  mv proof.json mintNBurnProof.json
+  cat mintNBurnProof.json
 fi
