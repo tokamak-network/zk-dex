7411307cb8988b21ff0afaef490cf5dc7547410e
4000D <dkdkajej@gmail.com>
2019-07-21 03:37:45 -0400

    vpp: remove unused directory

--- vapp/contracts/test/MockDai.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.24;
-
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
-
-contract MockDai is ERC20 {
-  constructor() public {
-    _mint(msg.sender, 100 * 10**18);
-  }
-}
